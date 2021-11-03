31a8d087055a544a5826294b33cfba973791c865
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2021-11-03 17:39:30 -0300

    Adicionado as outras rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,23 @@ import Route from "./Route";
 import Login from "../pages/Login";
 import Cadastro from "../pages/Cadastro";
 import Dashboard from "../pages/Dashboard";
+import Memorize from "./pages/Memorize/index";
+import Baralho from "./pages/Baralho/index.jsx";
+import Exame from "./pages/Exame/index";
+import Texto1 from "./pages/Textos/index.jsx";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route exact path="/cadastro" component={Cadastro} />
-      <Route exact path="/dashboard" component={Dashboard} isPrivate/>
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
+
+      <Route exact path="/textos" component={Texto1} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/memorize" component={Memorize} />
+      <Route exact path="/baralho/:id" component={Baralho} />
+      <Route exact path="/exame/:id" component={Exame} />
     </Switch>
   );
 }
