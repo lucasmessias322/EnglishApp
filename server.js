a966dbc3f0c94776a28d3231cdc987bfeb71cfb8
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2022-02-04 18:15:19 -0300

    5

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
+
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
-  app;
-  use(express.static("build"));
+  app.use(express.static("build"));
   app.get("*", (req, res) => {
     req.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
-
-  app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log("Server running on port: " + port);
-  });
 }
+
+app.listen(port, (err) => {
+  if (err) {return console.log(err)};
+
+  console.log("Server running on port: " + port);
+});
