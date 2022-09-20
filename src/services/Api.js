90f49ed4d35504b09e37b676155515a556a541b4
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2022-09-20 16:11:32 -0300

    update api url

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/Api",
+  baseURL: "https://textoingles-api.herokuapp.com/api",
 });
 
 export async function getTextos() {
